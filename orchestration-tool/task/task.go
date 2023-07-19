6a49b1257eb74a14dcc6c71d1ceac2ed6e515266
shankalpaLamichhane <shankalpa.lamichhane@gmail.com>
2023-07-18 19:22:04 -0500

    added config struct for starting containers as tasks

--- orchestration-tool/task/task.go
+++ orchestration-tool/task/task.go
@@ -36,3 +36,16 @@ type TaskEvent struct {
 	Timestamp time.Time
 	Task Task
 }
+
+type Config struct {
+	Name string
+	AttachStdin bool
+	AttachStdout bool
+	AttachStderr bool
+	Cmd []string
+	Image string
+	Memory int64
+	Disk int64
+	Env []string
+	RestartPolicy string
+}
